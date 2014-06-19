90c9b099e461e2259ad91d18bf038085eeba84e2
bitw <endval@gmail.com>
2014-06-19 10:15:31 +0700

    update

---
+++ src/views/ulogin.blade.php
@@ -0,0 +1,3 @@
+uLogin
+<script src="//ulogin.ru/js/ulogin.js"></script>
+<a href="#" id="uLogin" data-ulogin="display=window;fields=first_name,last_name;redirect_uri=http%3A%2F%2Flarapp%2Fulogin"><img src="https://ulogin.ru/img/button.png" width=187 height=30 alt="МультиВход"/></a>
