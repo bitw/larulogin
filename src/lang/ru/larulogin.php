06c777ddb66e21599e867d40375e7cc1a3c29f98
bitw <endval@gmail.com>
2014-06-23 14:06:45 +0700

    update

---
+++ src/lang/ru/larulogin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 22.06.14
+ * Time: 22:45
+ */
+
+return array(
+    'error' => 'Ошибка',
+    'email_already_registered' => 'Пользователь с таким адресом эл.почты уже существует',
+    'token expired' => 'Время действия ключа истекло.',
+);
